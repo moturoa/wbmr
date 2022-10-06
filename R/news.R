58df35254e2721484e9032fc28f7fc789e286ffd
Remko Duursma <remko@shintolabs.nl>
2022-10-06 11:52:13 +0200

    bug fix

--- R/news.R
+++ R/news.R
@@ -6,7 +6,7 @@ read_news <- function(tenant = NULL){
   data <- tools:::.build_news_db_from_package_NEWS_md("NEWS.md")
   
   if(!is.null(tenant)){
-    data <- subset(data, Category %in% c("", tenant))
+    data <- dplyr::filter(data, Category %in% c("", tenant))
   }
   
   data
